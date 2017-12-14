bf5d1c3036add86fc003d15f1cf85c9b64c48619
[Nabila] <[nazrulislamdobon@gmail.com]>
2017-12-13 20:24:34 -0500

    This is Bangldadesh project I have done some changes

---
+++ src/manikganj/MyHome.java
@@ -0,0 +1,19 @@
+package manikganj;
+
+public class MyHome {
+
+	public static void main(String[] args) {
+
+    System.out.println("Manikganj is my home district ");		
+    System.out.print("I was born there in sadar upojela");
+    System.out.print("\nI really miss  my home");
+    
+    
+    System.out.print("I was born there in sadar upojela");
+    System.out.print("\nI really miss  my home");
+    
+    
+    
+	}
+
+}
